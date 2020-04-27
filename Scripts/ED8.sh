47e7d89ac537cc48019c4e30bce519acbf509fda
HeleneKretzmer <61309241+HeleneKretzmer@users.noreply.github.com>
2020-04-27 17:23:49 +0200

    Update ED8.sh

--- Scripts/ED8.sh
+++ Scripts/ED8.sh
@@ -2,7 +2,7 @@
 ```{r}
 require(ggplot2)
 
-data <- read.table('Eed_Rnf2_CGImethylation_vs_WT.tsv', header=T)
+data <- read.table('ED8a_Eed_Rnf2_CGImethylation_vs_WT.tsv', header=T)
 
 ggplot(data, aes(x=Eed_Epi,y=Rnf2_Epi)) + theme_classic() + ylim(c(-0.5,0.5)) + xlim(c(-0.5,0.5)) + geom_vline(xintercept=c(-0.1,0,0.1)) + geom_hline(yintercept=c(-0.1,0,0.1)) + geom_point(size=1, aes(color=delta_Epi)) + scale_color_manual(values=c('cornflowerblue','lightsalmon','grey','cornflowerblue','blue','purple','lightsalmon','purple','red3')) + theme(legend.position='none') + theme(legend.position='none')
 
