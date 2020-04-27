180d8a8b164f0ddb7c808ed68015f0942389b2cb
HeleneKretzmer <61309241+HeleneKretzmer@users.noreply.github.com>
2020-04-27 17:13:17 +0200

    Update ED5.sh

--- Scripts/ED5.sh
+++ Scripts/ED5.sh
@@ -3,7 +3,7 @@
 require(ggplot2)
 require(RColorBrewer)
 
-data <- read.table('RepeatFamily_methylation.tsv', header=T)
+data <- read.table('ED5a_RepeatFamily_methylation.tsv', header=T)
 
 ggplot(melt(data), aes(x=variable, y=value)) + geom_point(aes(color=Family), shape=4, size=2) + theme_classic() + theme(axis.text.x=element_text(angle=90)) + facet_grid(~gsub('.*_','',variable), scales='free_x', space='free_x') + ylim(c(0,1)) + scale_color_manual(values=c(brewer.pal(6,'Blues')[-c(1,2)], brewer.pal(3,'Reds')[-1]))
 ```
@@ -14,7 +14,7 @@ ggplot(melt(data), aes(x=variable, y=value)) + geom_point(aes(color=Family), sha
 require(ggplot2)
 require(RColorBrewer)
 
-data <- read.table('RepeatFamily_expression.tsv', header=T)
+data <- read.table('ED5a_RepeatFamily_expression.tsv', header=T)
 
 ggplot(data, aes(x=type, y=avg_expr, color=element)) + geom_point(shape=4, size=2) + theme_classic() + facet_grid(~tissue) + theme(axis.text.x=element_text(angle=90, hjust=1)) + ylim(c(0,0.015)) + scale_color_manual(values=c(brewer.pal(6,'Blues')[-c(1,2)], brewer.pal(3,'Reds')[-1]))
 ```
@@ -24,7 +24,7 @@ ggplot(data, aes(x=type, y=avg_expr, color=element)) + geom_point(shape=4, size=
 ```{r}
 require(vioplot)
 
-data <- read.table('IAPEz-int_methylation.tsv', header=T)
+data <- read.table('Fig2e.ED5b_IAPEz-int_methylation.tsv', header=T)
 
 vioplot(data[, grep('Epi', colnames(data))], names=c(colnames(data[, grep('Epi', colnames(data))],)), cex=0.5, main='IAPEz-int')
 vioplot(data[, grep('Exe', colnames(data))], names=c(colnames(data[, grep('Exe', colnames(data))],)), cex=0.5, main='IAPEz-int')
@@ -35,7 +35,7 @@ vioplot(data[, grep('Exe', colnames(data))], names=c(colnames(data[, grep('Exe',
 ```{r}
 require(ggplot2)
 
-data <- read.table('IAPEz-int_expression.tsv', header=T)
+data <- read.table('Fig2e.ED5b_IAPEz-int_expression.tsv', header=T)
 
 ggplot(data, aes(x=type, y=fraction)) + geom_point(shape=4, size=2) + theme_classic() + facet_grid(~tissue) + theme(axis.text.x=element_text(angle=90, hjust=1)) + ylim(c(0,0.015))
 ```
@@ -45,7 +45,7 @@ ggplot(data, aes(x=type, y=fraction)) + geom_point(shape=4, size=2) + theme_clas
 ```{r}
 require(ggplot2)
 
-data <- read.table('L3mbtl2_methyl_expr.tsv', header=T)
+data <- read.table('Fig2g.ED5c_L3mbtl2_methyl_expr.tsv', header=T)
 
 ggplot() +
 geom_hline(yintercept=c(-0.2,0.2), color='grey') + geom_vline(xintercept=c(-0.1,0.1), color='grey') + theme_classic() +
@@ -60,7 +60,7 @@ geom_text(data=subset(data, XE=="X" & delta_Epi <= -0.1 & diff >= 0.2), aes(x=de
 ```{r}
 require(vioplot)
 
-data <- read.table('L3mbtl2_hypo_derepr_genes_methylation.tsv', header=T, row.names=1)
+data <- read.table('ED5e_L3mbtl2_hypo_derepr_genes_methylation.tsv', header=T, row.names=1)
 
 vioplot(data, names=colnames(data))
 ```
@@ -70,7 +70,7 @@ vioplot(data, names=colnames(data))
 ```{r}
 require(ggplot2)
 
-data <- read.table('L3mbtl2_hypo_derepr_genes_pos_cells.tsv', header=T)
+data <- read.table('ED5e_L3mbtl2_hypo_derepr_genes_pos_cells.tsv', header=T)
 
 ggplot(data, aes(x=KO, y=pos.cells)) + geom_boxplot() + theme_classic() + facet_wrap(~XE) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 ```
