684e139506788ce99b71b0443b710e2a1fc2ae78
HeleneKretzmer <61309241+HeleneKretzmer@users.noreply.github.com>
2020-04-27 17:31:22 +0200

    Update ED11.sh

--- Scripts/ED11.sh
+++ Scripts/ED11.sh
@@ -3,7 +3,7 @@
 require(ggplot2)
 require(scales)
 
-data <- read.table('Nanostring.tsv', header=T)
+data <- read.table('Fig4f.ED11c_Nanostring.tsv', header=T)
 
 data$gene <- factor(data$gene, levels = rev(c('Dppa3', 'Esrrb', 'Prdm1', 'Prdm14', 'Pou5F1', 'Nanos3', 'Sox2', 'Nanog', 'Dnd1', 'Pax6', 'Sox1', 'Nes', 'Dlx5', 'Eya', 'Sox3', 'Foxd3', 'Eomes', 'T', 'Mixl1', 'Evx1', 'Lhx1', 'Mesp1', 'Dll1', 'Tbx6', 'Noto', 'Shh', 'Gsc', 'Kdr', 'Foxf1', 'Tbx4', 'Tbx20', 'Postn', 'Gata4', 'Gata6', 'Foxa2', 'Sox17', 'Elf5', 'Bmp4', 'Bmp8b', 'Fgf5', 'Fgf4', 'Wnt3a', 'Nodal', 'Notch1')))
 data$condition <- factor(data$condition, levels = c('2i','24hFgf', '72hFgf','RA', 'LDN', 'XAV', 'SB', 'B5', 'B500', 'W10', 'W100', 'W1000','A10', 'A1000','W.A'))
@@ -28,8 +28,8 @@ require(ggplot2)
 require(scales)
 require(ggsignif)
 
-data <- read.table('/project/scRNA/SourceData/Nanostring_means.tsv', header=T)
-sig <- read.table('/project/scRNA/SourceData/Nanostring_sig.tsv', header=T)
+data <- read.table('ED11d_Nanostring_means.tsv', header=T)
+sig <- read.table('ED11d_Nanostring_sig.tsv', header=T)
 
 data$gene <- factor(data$gene, levels = c('Dppa3','Esrrb','Pax6', 'Sox1','T','Tbx20','Bmp4', 'Bmp8b','Gata4', 'Gata6'))
 data$condition <- factor(data$condition, levels = c('2i','24hFgf', '72hFgf','RA', 'LDN', 'XAV', 'SB', 'B5', 'B500', 'W10', 'W100', 'W1000','A10', 'A1000','W.A'))
